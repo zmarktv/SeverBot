025f2268e50c7b159888f9ab1ee08d61dba42e2e
zmarktv <42383258+zmarktv@users.noreply.github.com>
2018-08-14 22:45:22 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,57 +1,52 @@
 <?php
-//created by Pol.Cpl khomsan khemthong
-$access_token ='TuXlGZthdDeOMPs+tmjfjpXxj1338eDJSrYCuyfF7sbbK/V5+M+pZn7V9AjlrCzvn+A03/PyQSSoODoNu+ZpGnAUfxhOD14NKpT5XRbQzvb9uBqjwjuhiekVDgW8iZys/7Pl/VJLU8J8NnyH6GbF6AdB04t89/1O/w1cDnyilFU=';
-
-
-// Get POST body content
-$content = file_get_contents('php://input');
-// Parse JSON
-$events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			//source
-     			$userId = $event['source']['userId'];
-
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-	   		$receivetext = $event['message']['text'];
-
-      			$processtext = 'Sanny Bot'."\n";
-			$processtext .= $receivetext;
-
-		 	 // Build message to reply back
-	    		$messages = [
-	   		'type' => 'text',
-	    		'text' => $processtext
-	     		];
-
-			$url = 'https://api.line.me/v2/bot/message/push';
-			$data = [
-			'userId' => $userId,
-			'messages' => [$messages],
-			];
-
-      $post = json_encode($data);
-      $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-      $ch = curl_init($url);
-      curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-      curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-      curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-      curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-      $result = curl_exec($ch);
-      curl_close($ch);
-
-      echo $result . "\r\n";
-
-
+    $accessToken = "TuXlGZthdDeOMPs+tmjfjpXxj1338eDJSrYCuyfF7sbbK/V5+M+pZn7V9AjlrCzvn+A03/PyQSSoODoNu+ZpGnAUfxhOD14NKpT5XRbQzvb9uBqjwjuhiekVDgW8iZys/7Pl/VJLU8J8NnyH6GbF6AdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+    
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+    
+    //รับข้อความจากผู้ใช้
+    $message = $arrayJson['events'][0]['message']['text'];
+#ตัวอย่าง Message Type "Text"
+    if($message == "สวัสดี"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+        replyMsg($arrayHeader,$arrayPostData);
     }
-  }
-}
-echo "OK";
-
+    #ตัวอย่าง Message Type "Sticker"
+    else if($message == "ฝันดี"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "sticker";
+        $arrayPostData['messages'][0]['packageId'] = "2";
+        $arrayPostData['messages'][0]['stickerId'] = "46";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
+    #ตัวอย่าง Message Type "Text + Sticker ใน 1 ครั้ง"
+    else if($message == "ลาก่อน"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "อย่าทิ้งกันไป";
+        $arrayPostData['messages'][1]['type'] = "sticker";
+        $arrayPostData['messages'][1]['packageId'] = "1";
+        $arrayPostData['messages'][1]['stickerId'] = "131";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
+function replyMsg($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/message/reply";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
+   exit;
 ?>
